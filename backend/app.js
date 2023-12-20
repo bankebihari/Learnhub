a6deeca11aa902069b2b6be3ace63fe4dd0a761c
harshraj29 <rajh3282@gmail.com>
2023-12-20 20:27:57 +0530

    Error fixed

--- backend/app.js
+++ backend/app.js
@@ -20,7 +20,13 @@ app.use((req, res, next) => {
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
   res.header('Access-Control-Allow-Credentials', 'true');
-  next();
+
+  // Respond to preflight requests
+  if (req.method === 'OPTIONS') {
+    res.sendStatus(200);
+  } else {
+    next();
+  }
 });
 
 
