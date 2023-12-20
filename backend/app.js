17f5770e43e2293559faea3a823ffd4429c9f716
harshraj29 <rajh3282@gmail.com>
2023-12-20 20:34:44 +0530

    fixed

--- backend/app.js
+++ backend/app.js
@@ -15,7 +15,8 @@ configDotenv();
 
 app.use((req, res, next) => {
   const allowedOrigin = process.env.CLIENT_URL;
-  
+  console.log('Allowed Origin:', allowedOrigin);
+
   res.header('Access-Control-Allow-Origin', allowedOrigin);
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
@@ -28,6 +29,7 @@ app.use((req, res, next) => {
     next();
   }
 });
+
 
 
 // Middleware
