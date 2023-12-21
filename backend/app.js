6ccf55363a0d998254dea632c8533a21202c0673
harshraj29 <rajh3282@gmail.com>
2023-12-21 15:03:04 +0530

    Port changed

--- backend/app.js
+++ backend/app.js
@@ -17,7 +17,7 @@ app.use((req, res, next) => {
   const allowedOrigin = process.env.CLIENT_URL;
   console.log('Allowed Origin:', allowedOrigin);
 
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Origin', allowedOrigin);
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
   res.header('Access-Control-Allow-Credentials', 'true');
