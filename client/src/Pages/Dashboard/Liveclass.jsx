9d8387985b73fa0ab68baba0d1d63ce07d73adc5
harshraj29 <rajh3282@gmail.com>
2024-01-01 01:54:42 +0530

    Changed the live class and displaylecture ui

--- client/src/Pages/Dashboard/Liveclass.jsx
+++ client/src/Pages/Dashboard/Liveclass.jsx
@@ -4,10 +4,13 @@ import { useParams } from 'react-router-dom';
 
 function Liveclass() {
     const { lectureId } = useParams();
+
+    const userName = "Your Name";
+    
     const Liveclass = async(element)=> {
         const appID = 1752201823;
         const serverSecret = "a8e3273cfb32e052efb4b48febddf40f";
-        const kitToken =  ZegoUIKitPrebuilt.generateKitTokenForTest(appID, serverSecret, lectureId,  Date.now().toString(),  "Student");
+        const kitToken =  ZegoUIKitPrebuilt.generateKitTokenForTest(appID, serverSecret, lectureId,  Date.now().toString(),  userName);
          
         //create instance for live class
 
@@ -25,7 +28,7 @@ function Liveclass() {
     };
        
   return (
-    <div >
+    <div className='text-white'>
         <div ref={Liveclass} className='w-screen h-screen'/>
     </div>
   )
